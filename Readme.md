584ac82d79bc49b8d283a30b21bdecec3cc9598c
poa <Andreas.poetscher@htl-leoben.at>
2021-02-02 21:56:36 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+vpl-junit
+=========
+
+Purpose
+-------
+This project modifiers a .mbz file with predifined content.
+
+
+
---
+++ src/mbzModifier/mbzModifier.java
@@ -0,0 +1,12 @@
+package mbzModifier;
+
+public class mbzModifier {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Start mbz Modifier");
+		
+		
+		
+	}
+}
